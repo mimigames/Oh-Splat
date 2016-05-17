1e4382c9bb329855d7a67c2e899a289ba25ed257
bryanzantvoort <bryan.zantvoort@gmail.com>
2016-05-17 23:18:54 +0200

    Bird now flies in a straight line over a test street

---
+++ Oh Splat/Assets/Scripts/BirdControls.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdControls : MonoBehaviour {
+
+
+    public float forwardSpeed;
+    public float strafeSpeed;
+
+    private GameObject body;
+
+    private float horizontalAxis;
+
+    // Use this for initialization
+    void Start() {
+        body = transform.FindChild("Body").gameObject;
+        horizontalAxis = Input.GetAxis("Horizontal");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.Translate(Vector3.forward * Time.deltaTime * forwardSpeed);
+
+        if (horizontalAxis > 0) {
+            body.transform.Translate(Vector3.right * Time.deltaTime * strafeSpeed);
+        }
+        else if (horizontalAxis < 0) {
+            body.transform.Translate(Vector3.left * Time.deltaTime * strafeSpeed);
+        }
+    }
+}
